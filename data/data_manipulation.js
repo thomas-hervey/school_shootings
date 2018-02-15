fe25e54ec1597e1b5366f6c4c0f1bbc2770c39be
Thomas Hervey <thomasahervey@gmail.com>
2018-02-15 08:43:59 -0800

    finished: geocoding locations

--- data/data_manipulation.js
+++ data/data_manipulation.js
@@ -9,8 +9,6 @@ const _1990s = wiki_shootings[15]
 const _2000s = wiki_shootings[16]
 const _2010s = wiki_shootings[17]
 
-
-const exampleArray = _1990s.slice(0,2)
 var options = {
   provider: 'google',
 
@@ -21,58 +19,35 @@ var options = {
 
 var geocoder = NodeGeocoder(options);
 
+let rangeToUpdate = _1990s
 
-const locations = _1990s.map(shooting_instance => {
+const locations = rangeToUpdate.map(shooting_instance => {
   const location = shooting_instance['Location']
   return exampleLocation = location.split('!')[0].trim()
 })
-const example = ['Brooklyn, New York', 'test']
 
-const newValues = geocoder.batchGeocode(locations, (error, results) => {
+
+geocoder.batchGeocode(locations, (error, results) => {
   if(error) {
     console.log(error)
   }
   else {
-    return results.map((result, index) => {
+    const updatedResults = results.map((result, index) => {
       const lat = result['value'][0]['latitude']
       const lon = result['value'][0]['longitude']
       const coords = {
         lat: lat,
         lon: lon
       }
-      return _1990s[index].coords = coords
+      rangeToUpdate[index].coords = coords
+      return JSON.stringify(rangeToUpdate[index])
     })
+
+    // once values are updated, write them to a new file
+    fs.writeFile("./data/raw_data/wiki_shootings_with_coords.json", updatedResults.join(','), (err) => {
+      if (err) console.log(err)
+      else { console.log('successfully saved')}
+    })
+
   }
-
 })
-// TODO: resolve promise and update values of array locations
-
-console.log(newValues[0])
-
-
-// const updated_1990s = exampleArray.map(shooting_event => {
-
-//   // get the location attribute
-//   let exampleLocation = shooting_event['Location']
-//   exampleLocation = exampleLocation.split('!')[0]
-
-//   // Or using Promise
-//   const geocoded = geocoder.geocode(exampleLocation)
-//     .then(function(res) {
-//       console.log(res)
-//       const coords = {
-//         lat: res[0]['latitude'] || 0,
-//         lon: res[0]['longitude'] || 0
-//       }
-//       return res
-//     })
-//     .then(result => {
-//       shooting_event.coords = coords
-//       return shooting_event
-//     })
-//     .catch(function(err) {
-//       console.log(err)
-//     })
-// })
-
-// console.log(updated_1990s)
