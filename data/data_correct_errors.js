13efb08de33eb199493d084641fe3bf50139f710
Thomas Hervey <thomasahervey@gmail.com>
2018-03-01 15:29:39 -0800

    updated: data dates and values; added: metricsGraphics testing

--- data/data_correct_errors.js
+++ data/data_correct_errors.js
@@ -1,17 +1,47 @@
 const fs = require('fs')
+var dateFormat = require('dateformat')
 const compiled = require('./raw_data/compiled.json')
+const compiled_updated = require('./raw_data/compiled_updated.json')
 
 function updateDates(valuesToUpdate) {
-  const updatedResults = valuesToUpdate.map(result => {
+  return valuesToUpdate.map(result => {
     const resultToUpdate = result
     if (result.Date.startsWith('00000000') && result.Date.length >= 23) {
       resultToUpdate.Date = result.Date.substring(23)
     }
     return JSON.stringify(resultToUpdate)
   })
+}
 
+function updateCasualties(valuesToUpdate) {
+  return valuesToUpdate.map(result => {
+    const resultToUpdate = result
+    if (result.Deaths.startsWith('0,000,001') && result.Deaths.length >= 10) {
+      resultToUpdate.Deaths = parseInt(result.Deaths.substring(9))
+    }
+    if (result.Injuries.startsWith('0,000,001') && result.Injuries.length >= 10) {
+      resultToUpdate.Injuries = parseInt(result.Injuries.substring(9))
+    }
+
+    return resultToUpdate
+  })
+}
+
+function changeDates(valuesToUpdate) {
+  return valuesToUpdate.map(result => {
+    if (result.Date) {
+      const newDate = new Date(result.Date)
+      dateFormat
+      result.fullDate = dateFormat(newDate, "isoDate")
+      result.yearMonthDate = result.fullDate.substring(0, 7)
+    }
+    return JSON.stringify(result)
+  })
+}
+
+function writeResults(updatedResults) {
   try {
-    fs.writeFile("/Users/Thomas/Projects/personal/school_shootings/data/raw_data/compiled_updated.json", updatedResults.join(','), (err) => {
+    fs.writeFile("/Users/Thomas/Projects/personal/school_shootings/data/raw_data/compiled_updated_4.json", updatedResults.join(','), (err) => {
       if (err) console.log(err)
       else { console.log('successfully saved')}
     })
@@ -20,5 +50,5 @@ function updateDates(valuesToUpdate) {
   }
 }
 
-updateDates(compiled)
-
+const updatedResults = changeDates(updatedResults) // updateDates(compiled) // updateCasualties(compiled_updated)
+writeResults(updatedResults)
